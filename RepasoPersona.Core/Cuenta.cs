9d6eb1e61a3cfb2be1cfcb41916e6998e425160b
JavierMerele <javiermereleet12d1@gmail.com>
2023-03-16 15:36:39 -0300

    arreglos del tp1

--- RepasoPersona.Core/Cuenta.cs
+++ RepasoPersona.Core/Cuenta.cs
@@ -1,27 +1,29 @@
-namespace Dinero.Core;
-public abstract class Dinero
+namespace RepasoPersona.Core;
+public class Cuenta : Dinero
 {
-    public double Monto { get; protected set; }
+    private static int _contador;
+    public double Saldo { get; private set; }
+    public int CBU { get; private set; }
+    public Cuenta() => Saldo = 0;
+    public double Monto { get; private set; }
 }
 
-public Dinero(double monto)
+public Persona(double saldo, double Monto)
 {
+    CBU = ++_contador;
+    Saldo = saldo;
     Monto = monto;
 }
-
 
 public void Debitar(double monto)
 {
     if (monto <= 0)
         throw new ArgumentException("El monto tiene que ser mayor a cero.");
 
-
     if (monto > Saldo)
         throw new InvalidOperationException("El monto supera el saldo.");
     Saldo -= monto;
 }
-
-
 public void Acreditar(double monto)
 {
     if (monto <= 0)
